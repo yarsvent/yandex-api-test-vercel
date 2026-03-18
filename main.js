ad804e747ee95d13d034c42b5e2b1baa6df62108
Yaroslav Sventsitskiy <yaroslav.sventsitskiy@yandex.ru>
2026-03-18 22:42:09 +0300

    Add marker.

--- main.js
+++ main.js
@@ -21,8 +21,6 @@ var rand = {};
     return Math.random() * (max - min) + min;
   }
 
-await const {YMapDefaultMarker} = ymaps3.import('@yandex/ymaps3-markers@0.0.1');
-
 setTimeout(initMap, 500);
 
 function random_x() {
@@ -64,6 +62,8 @@ async function initMap() {
       });
       map.addChild(layer);
 
+      const {YMapDefaultMarker} = await import('@yandex/ymaps3-default-ui-theme');
+      
       const markerElement = document.createElement('div');
       markerElement.className = 'marker-class';
       markerElement.innerText = "I'm marker!";
