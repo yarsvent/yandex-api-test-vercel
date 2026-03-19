daccec4dcc85f5d22e02a34fb5833d85409838d7
Yaroslav Sventsitskiy <yaroslav.sventsitskiy@yandex.ru>
2026-03-19 16:09:35 +0300

    Change map size.

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ function initMap() {
   function init() {
     var myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
+        yandexMapDisablePoiInteractivity: true,
         zoom: 7
     });
     myMap.controls.remove('geolocationControl'); // удаляем геолокацию
@@ -68,6 +69,10 @@ function displayPanorama() {
       }
     }
   );
+}
+
+function nextLocation() {
+
 }
 
 // OLD V3 Code
