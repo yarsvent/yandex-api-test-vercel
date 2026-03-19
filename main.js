ac2c07644a338b1788ee84f7192e54153d60ee73
Yaroslav Sventsitskiy <yaroslav.sventsitskiy@yandex.ru>
2026-03-19 15:44:36 +0300

    Change behaviors.

--- main.js
+++ main.js
@@ -32,19 +32,14 @@ setTimeout(initMap, 1);
 function initMap() {
   ymaps.ready(init);
   function init() {
-    // Создание карты.
     var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
         center: [55.76, 37.64],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
+        behaviors: ['drag', 'pinchZoom', 'mouseTilt'],
         zoom: 7
     });
   }
 }
+
 // OLD V3 Code
 /*
 setTimeout(initMap, 1);
