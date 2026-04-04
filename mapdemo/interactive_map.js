e2c64779d3f009cd74da4ed79ae412aef7be627a
Yaroslav Sventsitskiy <yaroslav.sventsitskiy@yandex.ru>
2026-04-04 13:55:36 +0300

    Change name and init.

--- mapdemo/interactive_map.js
+++ mapdemo/interactive_map.js
@@ -41,7 +41,7 @@ async function initMap() {
         locationMarkers[place.name] = document.createElement('div');
         const markerElement = locationMarkers[place.name];
         markerElement.className = 'map-marker';
-        markerElement.innerHTML = "<span style=\"background-image: url('mapdemo/flags/"+place.flag+"')\" "+"class=\"map-flag\"></span>"+place.full_name;
+        markerElement.innerHTML = "<span style=\"background-image: url('mapdemo/flags/"+place.flag+"')\" "+"class=\"map-flag\"></span>"+place.name;
 
         const marker = new ymaps3.YMapMarker(
           {
